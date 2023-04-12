09f999a7e321d9d491b3f8d3c8a1d9fa1e49ff57
Raspberry LB <l.bartolini02@gmail.com>
2023-04-12 11:53:23 +0200

    formatted file

--- Lezione7/forkfigli.c
+++ Lezione7/forkfigli.c
@@ -1,27 +1,32 @@
 #include <stdio.h>
-int main (void) {
-  int pid;
-  pid = fork ();
-  if (pid != 0) { // parent
-    printf ("PID %d and PPID %d.\n", getpid (), getppid ());
-    printf ("My child’s PID is %d.\n", pid);
-    sleep(5);
-    int pid = fork();
-    if(pid != 0){ // parent
-        sleep(15); 
-        wait(NULL);
-        sleep(15);
-    }else{ // child 2
+int main(void)
+{
+    int pid;
+    pid = fork();
+    if (pid != 0)
+    { // parent
+        printf("PID %d and PPID %d.\n", getpid(), getppid());
+        printf("My child’s PID is %d.\n", pid);
         sleep(5);
-        printf ("PID %d and PPID %d.\n", getpid (), getppid ());
+        int pid = fork();
+        if (pid != 0)
+        { // parent
+            sleep(15);
+            wait(NULL);
+            sleep(15);
+        }
+        else
+        { // child 2
+            sleep(5);
+            printf("PID %d and PPID %d.\n", getpid(), getppid());
+        }
     }
-  }
-  else { // child 1
-    printf ("PID %d and PPID %d.\n", getpid (), getppid ());
-  }
+    else
+    { // child 1
+        printf("PID %d and PPID %d.\n", getpid(), getppid());
+    }
 
-  printf ("PID %d terminates.\n", getpid () ); /* everyome prints this */
+    printf("PID %d terminates.\n", getpid()); /* everyome prints this */
     exit(0);
-  return 0;
+    return 0;
 }
-
